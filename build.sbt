7ff61d4bbaa1a60e2d5ec98e77eede2808449cbc
Yuri Stul <yuriy.s@dauup-clicksmob.com>
2019-06-10 15:25:22 +0300

    XMLValidator

--- build.sbt
+++ build.sbt
@@ -3,10 +3,11 @@ lazy val scalaTestVersion = "3.2.0-SNAP10"
 lazy val scalaCheckVersion = "1.14.0"
 lazy val scalaMockVersion = "4.2.0"
 lazy val logbackVersion = "1.2.3"
+lazy val scalaXmlVersion = "1.2.0"
 
 lazy val commonSettings = Seq(
   organization := "com.stulsoft",
-  version := "1.0.1",
+  version := "1.1.0",
   scalaVersion := "2.12.8",
   scalacOptions ++= Seq(
     "-feature",
@@ -15,6 +16,7 @@ lazy val commonSettings = Seq(
   libraryDependencies ++= Seq(
     "com.typesafe.scala-logging" %% "scala-logging" % scalaLoggingVersion,
     "ch.qos.logback" % "logback-classic" % logbackVersion,
+    "org.scala-lang.modules" %% "scala-xml" % scalaXmlVersion,
     "org.scalatest" %% "scalatest" % scalaTestVersion % "test",
     "org.scalacheck" %% "scalacheck" % scalaCheckVersion % "test",
     "org.scalamock" %% "scalamock" % scalaMockVersion % "test"
---
+++ src/test/scala/com/stulsoft/scala/tools/xml/XMLValidatorTest.scala
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2019. Yuriy Stul
+ */
+
+/**
+  * @author Yuriy Stul
+  */
+package com.stulsoft.scala.tools.xml
+
+import com.stulsoft.scala.tools.resources.ResourceUtils
+import org.scalatest.{FunSuite, Matchers}
+
+class XMLValidatorTest extends FunSuite with Matchers {
+
+  test("testValidate valid") {
+    val xml = s"""<test>text</test>"""
+    XMLValidator.validate(xml).isSuccess shouldBe true
+  }
+
+  test("testValidate valid 2") {
+    val xml = ResourceUtils.source("xml1.xml").get.getLines().mkString
+    XMLValidator.validate(xml).isSuccess shouldBe true
+  }
+
+  test("testValidate invalid") {
+    val xml = s"""<test>text</testERROR>"""
+    XMLValidator.validate(xml).isSuccess shouldBe false
+  }
+
+  test("testValidate invalid 2") {
+    val xml = ResourceUtils.source("xml1ERR.xml").get.getLines().mkString
+    XMLValidator.validate(xml).isSuccess shouldBe false
+  }
+
+}
